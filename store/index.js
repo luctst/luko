f1900c07cf01be296f28d05bd3b84f596a5fc567
Lucas Tostée <lucas.tostee@gmail.com>
2019-10-20 20:58:42 +0200

    Add Vuex store

---
+++ store/index.js
@@ -0,0 +1,10 @@
+import Vuex from "vuex";
+
+export default () => {
+  return new Vuex.Store({
+    state: {
+      code: "SHARETHELOVE+5WMXM",
+      twitterApi: "https://api.twitter.com/1.1/"
+    }
+  })
+};
