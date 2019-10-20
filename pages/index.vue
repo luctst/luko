cac88735dd9d74a231265debd64d59c88c77039b
Lucas Tostée <lucas.tostee@gmail.com>
2019-10-20 20:57:43 +0200

    Create components routes

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <description></description>
+  </main>
+</template>
+
+<script>
+import description from "../components/description";
+
+export default {
+  components: {description}
+}
+</script>
+
+<style lang="scss" scoped>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0 15px;
+    margin: 0 auto;
+  }
+</style>
---
+++ pages/tweet.vue
@@ -0,0 +1,93 @@
+<template>
+  <main>
+    <section>
+      <h1>Ready To Tweet ? 🐦</h1>
+    </section>
+    <section class="tweet">
+      <input ref="authId"/>
+      <textarea :value="tweetContent" disabled></textarea>
+      <button @click="letsTweet">Tweet this 👆</button>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tweetContent: `Je vous partage mon code promo qui permet de bénéficier d'un mois gratuit chez la néo-assurance Luko : ${this.$store.state.code}`
+    }
+  },
+  methods: {
+    letsTweet() {
+
+    }
+  }
+}
+
+</script>
+<style lang="scss" scoped>
+  main {
+    font-family: Avenir;
+    padding: 0 15px;
+    margin: 0 auto;
+
+    section {
+      h1 {
+        text-align: center;
+        font-size: 2em;
+      }
+    }
+
+    .tweet {
+      display: flex;
+      flex-direction: column;
+
+      input {
+        border: none;
+        font-family: Avenir;
+        font-size: 16px;
+        font-weight: 500;
+        color: #0042da;
+        padding: 13px 24px;
+        border-radius: 4px;
+        background-color: #edf5fe;
+        width: 15em;
+      }
+
+      textarea {
+        color: #a5a4a4;
+        font-family: Avenir;
+        height: 128px;
+        font-size: 12px;
+        font-weight: 500;
+        line-height: 1.5;
+        resize: none;
+        padding: 12px 0 0 20px;
+        width: 97%;
+        border-radius: 4px;
+        border: solid 1px #dddddd;
+        width: 50%;
+        margin-top: 3em;
+      }
+
+      button {
+        height: 36px;
+        padding: 7px 24px;
+        border: none;
+        border-radius: 4px;
+        font-family: Avenir;
+        font-size: .8em;
+        font-weight: 900;
+        color: #0042da;
+        background-color: #edf5fe;
+        width: 15%;
+        margin-top: 1em;
+
+        &:hover {
+          cursor: pointer;
+        }
+      }
+    }
+  }
+</style>
