a4bcf5aa3bb5eca721114ac6bd75ecf6a7cb5e0c
Lucas Tostée <lucas.tostee@gmail.com>
2019-10-20 20:57:29 +0200

    Create components folder

---
+++ components/description.vue
@@ -0,0 +1,189 @@
+<template>
+  <section class="description">
+    <div class="description--img">
+      <img src="/emma-photo.png"/>
+    </div>
+    <div class="description--content">
+      <div class="description--content--info">
+        <h1>Emma</h1>
+        <p>Don’t forget that for each new subscriber you refer, both you and them get <span>one free month</span> of coverage! 💙</p>
+        <p> your <span>one free month</span> referral code</p>
+      </div>
+      <div class="description--content--btn">
+        <div class="input__btn">
+          <input :value="getCode" disabled/>
+          <button class="is__not__radius" v-clipboard="getCode" v-clipboard:success="copyToclipboard">
+            <img src="/copy-icon.svg"/>
+            Copy code
+          </button>
+        </div>
+        <nuxt-link to="/tweet">
+          <img src="/twitter-icon.svg"/>
+          Tweet
+        </nuxt-link>
+        <button>
+          <img src="/facebook-icon.png"/>
+          Share
+        </button>
+      </div>
+      <formData></formData>
+    </div>
+    <div v-if="showModal" class="modal">
+      <div>
+        <p>{{getCode}} has been copied !! <span>👍</span> closed in {{modalTime}}s</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Vue from "vue";
+import Clipboard from "v-clipboard";
+import formData from "./form";
+
+Vue.use(Clipboard);
+
+export default {
+  components: {formData},
+  data() {
+    return {
+      showModal: false,
+      modalTime: 5,
+      clearTimeout: null
+    }
+  },
+  computed: {
+    getCode() {
+      return this.$store.state.code
+    }
+  },
+  methods: {
+    copyToclipboard({value, event}) {
+      this.showModal = true;
+
+      this.clearTimeout = setInterval(() => this.modalTime = this.modalTime - 1, 1000);
+    }
+  },
+  updated() {
+    if (this.modalTime === 0 && this.clearTimeout !== null) {
+      clearInterval(this.clearTimeout);
+      this.showModal = false;
+      this.modalTime = 5;
+      this.clearTimeout = null;
+    };
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .description {
+    display: flex;
+    flex-direction: column;
+    height: 95vh;
+    margin-top: 4em;
+
+    span {
+      font-weight: 900;
+    }
+
+    &--img {
+      img {
+        object-fit: cover;
+        width: 48px;
+        height: auto;
+      }
+    }
+
+    &--content {
+      margin-left: 16px;
+
+      div {
+        h1 {
+          font-family: Avenir;
+          font-size: 16px;
+          font-weight: 900;
+          margin: 0 0 6px 0;
+        }
+        p {
+          font-family: Avenir-Roman;
+          font-size: 16px;
+          font-weight: normal;
+          line-height: 1.38;
+          margin: 0 15% 15px 0;
+        }
+      }
+
+      &--btn {
+        display: flex;
+
+        .input__btn {
+          display: flex;
+
+          input {
+            border: none;
+            font-family: Avenir;
+            font-size: 16px;
+            font-weight: 500;
+            color: #0042da;
+            padding: 13px 24px;
+            border-radius: 4px;
+            background-color: #edf5fe;
+            width: 15em;
+          }
+
+          .is__not__radius {
+            border-top-left-radius: 0;
+            border-bottom-left-radius: 0;
+
+            &:hover {
+              cursor: url("/copy-icon.svg"), auto;
+            }
+          }
+        }
+
+        a {
+          text-decoration: none;
+        }
+
+        button,
+        a {
+          display: flex;
+          align-items: center;
+          border: none;
+          border-radius: 4px;
+          background-color: #0042da;
+          margin-right: 1em;
+          font-family: Avenir;
+          font-size: 14px;
+          color: #fff;
+          padding: 1em 2em;
+
+          img {
+            height: auto;
+            width: 15%;
+            margin-right: 1em;
+          }
+        }
+      }
+    }
+  }
+
+  .modal {
+    width: fit-content;
+    margin: auto;
+    align-self: flex-end;
+
+    div {
+      padding: 14px 16px;
+      border-radius: 4px;
+      color: #fff;
+      background-color: #4caf50;
+
+      p {
+        font-family: Avenir, Helvetica, sans-serif;
+        font-size: .875rem;
+        margin: 0;
+      }
+    }
+  }
+</style>
---
+++ components/form.vue
@@ -0,0 +1,113 @@
+<template>
+  <section>
+    <p>
+      You can also send
+      <span>one free month</span> via email, separate emails with semicolon « ; »
+    </p>
+    <textarea :placeholder="textAreaExemple" @input="onChange"></textarea>
+    <div>
+      <button>
+        <img src="send-icon.svg" />
+        Send {{mailToSend}} mail
+      </button>
+      <button @click="displayOnClick">Done</button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      textAreaExemple: "amelie.dupont@gmail.com ; jeanmichel@gmail.com",
+      mailToSend: 0
+    };
+  },
+  methods: {
+    displayOnClick() {
+      console.log("Done well played !! 👏👏");
+    },
+    onChange(e) {
+      if (e.target.value === "") {
+          this.mailToSend = 0;
+      }
+
+      if (e.target.value !== "" && !e.target.value.includes(";")) {
+          this.mailToSend = e.target.value.split(';').length;
+      }
+
+      if (e.target.value.includes(";")) {
+        this.mailToSend = e.target.value.split(';').length;
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+section {
+  margin-top: 36px;
+
+  span {
+    font-weight: 900;
+  }
+
+  p {
+    font-family: Avenir-Roman;
+    font-size: 16px;
+    line-height: 1.38;
+  }
+
+  textarea {
+    color: #020202;
+    font-family: Avenir;
+    height: 128px;
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 1.5;
+    resize: none;
+    padding: 12px 0 0 20px;
+    width: 97%;
+    border-radius: 4px;
+    border: solid 1px #dddddd;
+  }
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+
+    button {
+      height: 36px;
+      padding: 7px 24px;
+      border: none;
+      border-radius: 4px;
+      font-family: Avenir;
+      font-size: 0.8em;
+      font-weight: 900;
+
+      &:hover {
+        cursor: pointer;
+      }
+    }
+
+    button:first-child {
+      display: flex;
+      align-items: center;
+      color: #fff;
+      background-color: #bdbfc3;
+
+      img {
+        height: auto;
+        width: 15%;
+        margin-right: 15px;
+      }
+    }
+
+    button:last-child {
+      color: #0042da;
+      background-color: #edf5fe;
+    }
+  }
+}
+</style>
