39ba049cc0de87a08a3ec975269ed87d4043620b
Lucas Tostée <lucas.tostee@gmail.com>
2019-10-21 16:51:20 +0200

    Create modal component

---
+++ components/modal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="modal">
+      <div>
+        <p>{{this.$store.state.code}} has been copied !! <span>👍</span> closed in {{time}}s</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["time"]
+}
+</script>
+
+<style lang="scss" scoped>
+.modal {
+    width: fit-content;
+    margin: auto;
+    align-self: flex-end;
+
+    div {
+      padding: 14px 16px;
+      border-radius: 4px;
+      color: #fff;
+      background-color: #4caf50;
+
+      p {
+        font-family: Avenir, Helvetica, sans-serif;
+        font-size: .875rem;
+        margin: 0;
+      }
+    }
+  }
+</style>
