3297ada53322c730ef7c883011ee85f98801343d
Lucas Tostée <lucas.tostee@gmail.com>
2019-10-21 16:52:05 +0200

    Add tweetContent

--- store/index.js
+++ store/index.js
@@ -4,7 +4,8 @@ export default () => {
   return new Vuex.Store({
     state: {
       code: "SHARETHELOVE+5WMXM",
-      twitterApi: "https://api.twitter.com/1.1/"
+      twitterApi: "https://api.twitter.com/1.1/",
+      tweetContent: "Je vous partage mon code promo qui permet de bénéficier d'un mois gratuit chez la néo-assurance Luko :SHARETHELOVE+5WMXM"
     }
   })
 };
